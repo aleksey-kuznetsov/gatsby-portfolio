701a6088460747585067195863fe86e996b52088
Aleksey Kuznetsov <akuznets@broadinstitute.org>
2021-01-14 15:23:43 +0300

    Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,28 @@
 module.exports = {
+  pathPrefix: 'gatsby-portfolio',
   siteMetadata: {
-    title: "gatsby-portfolio",
+    title: 'gatsby-portfolio',
+    siteUrl: 'https://aleksey-kuznetsov.github.io/gatsby-portfolio',
   },
   plugins: [
-    "gatsby-plugin-sass",
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    'gatsby-plugin-sass',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: "src/images/icon.png",
+        icon: 'src/images/icon.png',
       },
     },
-    "gatsby-transformer-sharp",
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
   ],
 };
